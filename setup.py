c4fe332fa4e8e75a0dfdde59b3a2137af527ab71
Opensource Technologies Consultancy Hub <info@opentch.com>
2019-04-23 18:15:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from setuptools import setup, find_packages
 #from pip.req import parse_requirements
 import re, ast
 
-# get version from __version__ variable in phtax/__init__.py
+# get version from version variable in phtax/init.py
 _version_re = re.compile(r'version\s+=\s+(.*)')
 
-#requirements = parse_requirements("requirements.txt", session="")
 with open('requirements.txt') as f:
-install_requires = f.read().strip().split('\n')
+    install_requires = f.read().strip().split('\n')
 
 with open('phtax/init.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
+
+#requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='phtax',
